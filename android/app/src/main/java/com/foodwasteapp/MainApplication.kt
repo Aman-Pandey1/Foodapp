3fa1fbdcc919afc86b301e8a314e025727278431
Aman-Pandey1 <amanpandeyacs@gmail.com>
2025-09-11 15:12:32 +0530

    [add]

--- android/app/src/main/java/com/foodwasteapp/MainApplication.kt
+++ android/app/src/main/java/com/foodwasteapp/MainApplication.kt
@@ -2,29 +2,38 @@ package com.foodwasteapp
 
 import android.app.Application
 import com.facebook.react.PackageList
-import com.facebook.react.ReactApplication
 import com.facebook.react.ReactNativeHost
 import com.facebook.react.ReactPackage
-import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint
+import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.load
 import com.facebook.react.defaults.DefaultReactNativeHost
 import com.facebook.soloader.SoLoader
 
-class MainApplication : Application(), ReactApplication {
-    private val mReactNativeHost: ReactNativeHost = object : DefaultReactNativeHost(this) {
-        override fun getPackages(): List<ReactPackage> = PackageList(this).packages
-        override fun getJSMainModuleName(): String = "index"
-        override fun isNewArchEnabled(): Boolean = BuildConfig.IS_NEW_ARCHITECTURE_ENABLED
-        override fun isHermesEnabled(): Boolean = BuildConfig.IS_HERMES_ENABLED
-    }
+class MainApplication : Application(), com.facebook.react.ReactApplication {
 
-    override fun getReactNativeHost(): ReactNativeHost = mReactNativeHost
+    override val reactNativeHost: ReactNativeHost =
+        object : DefaultReactNativeHost(this) {
+            override fun getUseDeveloperSupport(): Boolean =
+                BuildConfig.DEBUG
+
+            override fun getPackages(): List<ReactPackage> {
+                return PackageList(this).packages
+            }
+
+            override fun getJSMainModuleName(): String = "index"
+
+            // ✅ Correct property names
+            override val isNewArchEnabled: Boolean
+                get() = BuildConfig.IS_NEW_ARCHITECTURE_ENABLED
+
+            override val isHermesEnabled: Boolean
+                get() = BuildConfig.IS_HERMES_ENABLED
+        }
 
     override fun onCreate() {
         super.onCreate()
-        SoLoader.init(this, false)
+        SoLoader.init(this, /* native exopackage */ false)
         if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
-            DefaultNewArchitectureEntryPoint.load()
+            load()
         }
     }
 }
-
